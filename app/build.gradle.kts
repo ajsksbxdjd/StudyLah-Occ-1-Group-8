700019a05c5c71d38d29e96eafd6874819f9aaff
YuXuan0606 <teohyuxuan666@gmail.com>
2025-01-11 18:09:57 +0800

    Yu Xuan update register and login page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
+    implementation("com.android.volley:volley:1.2.1")
     implementation(libs.constraintlayout)
     implementation(libs.drawerlayout)
     implementation(libs.gridlayout)
@@ -44,9 +45,12 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
 
     implementation("androidx.appcompat:appcompat:1.6.0")
+    implementation("com.google.android.material:material:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.4")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
 
 }
