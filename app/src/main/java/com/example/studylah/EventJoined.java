dcb39d932e7a7fa62c10595a6ccbb7b6853bbd8a
LIKSHEN <likshen13579@gmail.com>
2025-01-12 16:51:03 +0800

    Added some logics in eventJoined

--- app/src/main/java/com/example/studylah/EventJoined.java
+++ app/src/main/java/com/example/studylah/EventJoined.java
@@ -1,12 +1,28 @@
 package com.example.studylah;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,17 +31,27 @@ import android.view.ViewGroup;
  */
 public class EventJoined extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private TextView eventNameDetails;
+    private TextView eventTutorDetails;
+    private TextView eventDate;
+    private TextView eventTime;
+    private TextView eventLocation;
+    private TextView eventDescription;
+    private TextView eventRegistrationLink;
+    private TextView eventTutorEmail;
+    private TextView eventWebsiteLink;
+    private ImageView eventImageDetails;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private RecyclerView joinedRecyclerView;
+    private EventAdapter eventAdapter;
+    private List<Event> joinedEventList = new ArrayList<>();
 
     public EventJoined() {
         // Required empty public constructor
+    }
+
+    public static EventJoined newInstance() {
+        return new EventJoined();
     }
 
     /**
@@ -34,31 +60,95 @@ public class EventJoined extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment EventJoined.
+     * @return A new instance of fragment joined.
      */
     // TODO: Rename and change types and number of parameters
     public static EventJoined newInstance(String param1, String param2) {
         EventJoined fragment = new EventJoined();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_event_joined, container, false);
+        View view = inflater.inflate(R.layout.fragment_event_joined, container, false);
+
+        // Initialize RecyclerView
+        joinedRecyclerView = view.findViewById(R.id.joinedRecyclerView);
+        joinedRecyclerView.setLayoutManager(new GridLayoutManager(getContext(),2));
+        eventAdapter = new EventAdapter(joinedEventList, getContext());
+        joinedRecyclerView.setAdapter(eventAdapter);
+
+        // Load joined events from SharedPreferences
+        loadJoinedEvents();
+
+        return view;
     }
+
+    private void loadJoinedEvents() {
+        SharedPreferences sharedPreferences = getContext().getSharedPreferences("joined_events", Context.MODE_PRIVATE);
+        String joinedEventsJson = sharedPreferences.getString("joined_events_list", "[]");
+
+        try {
+            JSONArray joinedEventsArray = new JSONArray(joinedEventsJson);
+            for (int i = 0; i < joinedEventsArray.length(); i++) {
+                JSONObject eventObject = joinedEventsArray.getJSONObject(i);
+
+                String id = eventObject.getString("event_id");
+                Log.d("joined", "Event ID: " + id);
+                String name = eventObject.getString("event_name");
+                String tutor = eventObject.getString("event_tutor");
+                String date = eventObject.getString("event_date");
+                String time = eventObject.getString("event_time");
+                String location = eventObject.getString("event_location");
+                String description = eventObject.getString("event_description");
+                String registrationLink = eventObject.getString("event_registration_link");
+                String tutorEmail = eventObject.getString("event_tutor_email");
+                String websiteLink = eventObject.getString("event_website_link");
+                String base64Image = eventObject.getString("event_base64_image");
+
+                // Log the base64Image string
+                Log.d("joined", "Base64 Image: " + base64Image);
+
+                // Decode the base64 image string to a Bitmap
+                Bitmap image = null;
+                if (base64Image != null && !base64Image.isEmpty()) {
+                    try {
+                        // Decode Base64 image
+                        image = decodeBase64Image(base64Image);
+                    } catch (IllegalArgumentException e) {
+                        e.printStackTrace();
+                        Log.e("joined", "Failed to decode base64 image: " + e.getMessage());
+                    }
+                } else {
+                    Log.e("joined", "Base64 image string is null or empty");
+                }
+
+                // Create an Event object and add it to the list
+//                joinedEventList.add(new Event(name, tutor, date + ", " + location, base64Image, date, time, location, description, websiteLink, registrationLink, tutorEmail));
+                Event event = new Event(id, name, tutor, date + ", " + location, base64Image, date, time, location, description, websiteLink, registrationLink, tutorEmail, false);
+                event.setJoined(true); // Mark as joined
+                joinedEventList.add(event);
+            }
+
+            // Notify the adapter of data changes
+            eventAdapter.notifyDataSetChanged();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Bitmap decodeBase64Image(String base64Image) {
+        byte[] decodedBytes = Base64.decode(base64Image, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
+    }
+
+
 }
