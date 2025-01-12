338ed3d86ea763565e53d17832ac159c8d322ed1
LIKSHEN <likshen13579@gmail.com>
2025-01-12 16:03:53 +0800

    Added a page after event feedback is submitted successfully

---
+++ app/src/main/java/com/example/studylah/EventFeedbackSubmitted.java
@@ -0,0 +1,30 @@
+package com.example.studylah;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EventFeedbackSubmitted extends AppCompatActivity {
+
+    Button BtnBackToEvent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_created);
+
+        BtnBackToEvent = (Button) findViewById(R.id.BtnBackToEvent);
+
+        BtnBackToEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), EventActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
